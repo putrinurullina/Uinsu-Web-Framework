64e860decfd192e31df93f0f56dff5853423a702
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-10-26 08:43:05 +0700

    Create rule.php

---
+++ src/engine/rule/rule.php
@@ -0,0 +1,59 @@
+<?php
+//----- Settingan umum -------//
+$homeBase = 'http://localhost/uinsuWf/';
+$siteName = 'Uinsu Web Framework';
+$author = 'Uinsu';
+$styleBase = $homeBase.'ladun/style/';
+$imgBase = $homeBase.'ladun/img/';
+//
+//
+//
+//
+//
+//
+//---- Database -----//
+$dbDriver = 'mysqli';
+$dbServer = 'localhost';
+$dbUser = 'root';
+$dbPassword = '';
+$dbName = 'dbs_haxors';
+//
+//
+//
+//
+//
+//
+//
+//------ Fungsi ------//
+$stringRandom = "QWERTYUIOPLKJHGFDSAZXCVBNMQWERTYUIOPLKJHGFDSAZXCVBNM";
+$numberRandom = "789456123";
+$allRandom = $stringRandom.$numberRandom;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+define('HOMEBASE',$homeBase);
+define('SITENAME',$siteName);
+define('AUTHOR',$author);
+define('STYLEBASE',$styleBase);
+define('IMGBASE',$imgBase);
+
+define('RANDOM_STRING',$stringRandom);
+define('NUMBER_RANDOM',$numberRandom);
+define('ALL_RANDOM',$allRandom);
+
+define('DB_DRIVER',$dbDriver);
+define('DB_SERVER',$dbServer);
+define('DB_USER',$dbUser);
+define('DB_PASSWORD',$dbPassword);
+define('DB_NAME',$dbName);
