4e0e6a0bc7c001eed7247ee89f2b793add44a58b
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-10-26 09:00:57 +0700

    Update default.bind.php

--- src/engine/bind/home/default.bind.php
+++ src/engine/bind/home/default.bind.php
@@ -1,1 +1,78 @@
-Welcome to UINSU Web Framework
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title><?=SITENAME; ?></title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <style type="text/css">
+          html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+            .full-height {
+                height: 100vh;
+            }
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+            .position-ref {
+                position: relative;
+            }
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+            .content {
+                text-align: center;
+            }
+            .title {
+                font-size: 84px;
+            }
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+            <div class="content">
+              Selamat menggunakan
+                <div class="title m-b-md">
+                    Uinsu Web Framework
+                </div>
+                <div>Ayo mulai dari jelajahi di -> "/engine/route/home.route.php"<br/>
+                  Setting konfigurasi web anda di -> "/engine/rule/rule.php"
+                </div><br/>
+                <div class="links">
+                    <a href="http://haxorsprogrammingclub.id/uinsu-web-framework" target="new">Documentation</a>
+                    <a href="https://github.com/haxorsprogramming/Uinsu-Web-Framework" target='new'>Git</a>
+                    <a href="https://github.com/haxorsprogramming/Uinsu-Web-Framework" target='new'>API</a>
+                    <a href="http://haxorsprogrammingclub.id/update">Update</a>
+                    <a href="http://haxorsprogrammingclub.id">HaxorsProgramming</a>
+                   
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
