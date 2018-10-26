e34658b563b8d4cf51da9a8d9a38b9aa810d3997
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-10-26 07:14:44 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,7 +3,12 @@
 //Framework ini masih dalam pengembangan
 //Berbagai fungsi akan ditambahkan untuk memperlengkap web framework ini
 //Developer : HaxorsProgrammingClub
-//Pengembangan Micro Framework ini mengikuti tutorial dari bapak sandika selaku pemilik channel yotube "Web Programming Unpas"
+//Pengembangan Core Framework ini mengikuti tutorial dari bapak sandika selaku pemilik channel yotube "Web Programming Unpas"
+//Tim Pengembang Uinsu Web Framework
+//Pembina -> Muhammad Ikhsan, ST. M.Kom
+//Core Project -> Aditia Darma Nasution
+//Ecosystem -> Adam Falizufa Sagara
+//Tester -> Abdillah
 
 
 require_once 'engine/init.php';
