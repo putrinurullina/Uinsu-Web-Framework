c360af87e3e34d6e443730f92bf1ddb0d1cbd0f0
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-10-26 07:11:38 +0700

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+//welcome to Uinsu Web Framework
+//Framework ini masih dalam pengembangan
+//Berbagai fungsi akan ditambahkan untuk memperlengkap web framework ini
+//Developer : HaxorsProgrammingClub
+//Pengembangan Micro Framework ini mengikuti tutorial dari bapak sandika selaku pemilik channel yotube "Web Programming Unpas"
+
+
+require_once 'engine/init.php';
+
+$props = new Props;
