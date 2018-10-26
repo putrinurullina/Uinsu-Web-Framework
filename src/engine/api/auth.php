d70febfaf7a006c1963c4731ab061663aa55b389
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-10-26 20:13:25 +0700

    Update auth.php

--- src/engine/api/auth.php
+++ src/engine/api/auth.php
@@ -1,1 +1,10 @@
+<?php
+$username = addslashes($_POST['username']);
+$password = addslashes($_POST['password']);
 
+class dataLogin{}
+
+$newDataLogin = new DataLogin;
+
+$newDataLogin -> username = $username;
+$newDataLogin -> password = $password;
