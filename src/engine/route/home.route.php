f5ad6d9e4ef98699dcfece00e0d94aa6982b4fa7
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-10-30 14:19:25 +0700

    Update home.route.php

--- src/engine/route/home.route.php
+++ src/engine/route/home.route.php
@@ -4,5 +4,11 @@ class Home extends Route{
     
     public function index(){
         $this -> bind('/home/default');
-    }    
+    }
+    
+    public function kontak()
+    {
+        $this -> bind ('/home/kontak');
+    }
+    
 }
