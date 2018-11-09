f633c78b8833c763227e5e011ce65890c2f803cb
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-11-09 08:56:48 +0700

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,23 +1,5 @@
 <?php
 
-//welcome to Uinsu Web Framework
-//Framework ini masih dalam pengembangan
-//Berbagai fungsi akan ditambahkan untuk memperlengkap web framework ini
-//Developer : HaxorsProgrammingClub
-//Pengembangan inti Framework ini mengikuti tutorial dari bapak Shandika Galih selaku pemilik channel yotube "Web Programming Unpas"
-//Tim Pengembang Uinsu Web Framework
-//Pembina -> Muhammad Ikhsan, ST. M.Kom
-//Core Project -> Aditia Darma Nasution
-//Ecosystem -> Adam Falizufa Sagara
-//Dokumentasi -> Diana Vita
-//Tester -> Abdillah
-
-
 require_once 'engine/init.php';
 $props = new Props;
 
-
-
-require_once 'engine/init.php';
-
-$props = new Props;
