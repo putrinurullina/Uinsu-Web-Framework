7cebc02a461a352aa00ddb60e70c7a4c55c36354
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-11-09 08:47:15 +0700

    Update homeSt.php

--- src/engine/state/homeSt.php
+++ src/engine/state/homeSt.php
@@ -13,9 +13,31 @@ class homeSt{
 
   public function mhsData()
   {
-   $this -> st -> query("SELECT * FROM " . $this -> tbl);
+   $this -> st -> query(DB_SELECT."* FROM " . $this -> tbl);
    return $this -> st -> queryAll();
 
+  }
+
+  public function mhsDetail($nim)
+  {
+    $this -> st -> query('SELECT * FROM '. $this -> tbl . ' WHERE nim=:nim');
+    $this -> st -> querySet('nim',$nim);
+    return $this -> st -> querySingle(); 
+  }
+
+  public function tambahData()
+  {
+    $nama = "Adit";
+    $nim = "0701163112";
+    $email = "adit@gmail.com";
+
+    $query = DB_INSERT.$this -> tbl ." VALUES ('',:nim,:nama,:email);";
+
+    $this -> st -> query($query);
+    $this -> st -> querySet('nim',$nim);
+    $this -> st -> querySet('nama',$nama);
+    $this -> st -> querySet('email',$email);
+    $this -> st -> queryRun();    
   }
   
   
