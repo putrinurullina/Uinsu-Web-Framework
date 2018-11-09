b8e0b336d97004064bfd0fb08a3479f0414489d7
putrinurullina <39446471+putrinurullina@users.noreply.github.com>
2018-11-09 10:12:54 +0700

    Update home.route.php

--- src/engine/route/home.route.php
+++ src/engine/route/home.route.php
@@ -9,6 +9,22 @@ class Home extends Route{
         $this -> bind('/home/default');
     }
     
+    public function dataMahasiswa()
+    {
+        $data['mhs'] = $this -> state('homeSt') -> mhsData();
+        
+        $this -> bind('/home/dataBind',$data);
+    }
+
+
+    public function dataSubMhs()
+    {
+        $adit = "Adit";
+        return $adit;
+    }
+
+
+
     public function detailMhs($nim)
     {
         $data['mhs'] = $this -> state('homeSt') -> mhsDetail($nim);
@@ -20,8 +36,21 @@ class Home extends Route{
         $data['nim'] = '0701163114';
         $data['nama'] = 'Aditia Darma';
         $data['email'] = 'alditha.forum@gmail.com';
-
         $this -> state('homeSt') -> tambahData();
+    }
+
+    public function proTambahData()
+    {
+        $data['nim'] = $this -> inputPost('txtNim');
+        $data['nama'] = $this -> inputPost('txtNama');
+        $data['email'] = $this -> inputPost('txtEmail');
+        $this -> state('homeSt') -> tambahData($data);
+        $this -> toSite('home/formTambah');
+    }
+
+    public function formTambah()
+    {
+        $this -> bind('/home/formTambah');
     }
 
     public function kontak()
