11fb68f3f16b7f0a0202b7df285f3a9169402546
deakhairunnisa <45210903+deakhairunnisa@users.noreply.github.com>
2018-11-21 01:03:24 +0700

    Update route.php

--- src/engine/core/route.php
+++ src/engine/core/route.php
@@ -37,6 +37,10 @@ class Route{
     
     public function emailCheck($email)
     {
-
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          echo("$email is a valid email address");
+        } else {
+          echo("$email is not a valid email address");
+        }
     }
 }
