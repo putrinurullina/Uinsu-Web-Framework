21caace170373b1a2e0bf0674086720509b5a985
Annastasya Purnama Nikita <31577022+annanikita@users.noreply.github.com>
2018-12-03 01:14:24 +0700

    Update default.bind.php

--- engine/bind/home/default.bind.php
+++ engine/bind/home/default.bind.php
@@ -7,12 +7,12 @@
         <title><?=SITENAME; ?> - Home</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+       <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
         <style type="text/css">
           html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Nunito', sans-serif;
+               font-family: 'Asap', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
@@ -61,17 +61,22 @@
                 <div class="title m-b-md">
                     Uinsu Web Framework
                 </div>
-                <div>Ayo mulai dari jelajahi di -> "/engine/route/home.route.php"<br/>
-                  Setting konfigurasi web anda di -> "/engine/rule/base.php"
+                <div>Ayo mulai dari jelajahi di => "/engine/route/home.route.php"<br/>
+                  Setting konfigurasi web anda di => "/engine/rule/base.php"
                 </div><br/>
                 <div class="links">
                     <a href="<?=HOMEBASE; ?>doc/index.html" target="new">Documentation</a>
                     <a href="https://github.com/haxorsprogramming/Uinsu-Web-Framework" target='new'>Git</a>
                     <a href="https://github.com/haxorsprogramming/Uinsu-Web-Framework" target='new'>API Development</a>
                     <a href="<?=HOMEBASE; ?>crud">Example CRUD</a>
-                    <a href="http://haxorsprogrammingclub.id">HaxorsProgramming</a>
+                    
                    
                 </div>
+                <br/>
+                <a href="http://uinsu.ac.id" target="new">
+                <img src="http://uinsu.ac.id/iain_cpanel/ckfinder2/userfiles/images/logo-uinsu.jpg" style="width:120px;"></a>
+                <a href="http://haxorsprogrammingclub.id" target="new">
+                <img src="http://haxorsprogrammingclub.id/ladun/img/default/logo_club.png" style="width:200px;"></a>
             </div>
         </div>
     </body>
