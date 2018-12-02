425e42ded2fe2ee88a7b239ed803f05edac6894d
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-12-02 20:49:17 +0700

    Add files via upload

---
+++ engine/core/route.php
@@ -0,0 +1,46 @@
+<?php
+
+class Route{
+    public function bind($blade, $data = [])
+    {
+        require_once 'engine/bind/'.$blade.'.bind.php';    
+    }
+
+    public function state($state)
+    {
+        require_once 'engine/state/'.$state.'.php';
+        return new $state;
+    }
+    
+    public function randomString($length)
+    {
+     $data['string'] = str_shuffle(BASE_STRING_64);
+    return $length;
+    }        
+    
+    public function uploadFoto($path,$newPath)
+    {
+        move_uploaded_files($path,$newPath);
+    }  
+    
+    public function inputPost($id)
+    {
+        $id = $_POST[$id];
+        return $id;
+    }
+
+    public function inputGet($id)
+    {
+        $id = $_POST[$id];
+        return $id;
+    }
+    
+    public function emailCheck($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          echo("$email is a valid email address");
+        } else {
+          echo("$email is not a valid email address");
+        }
+    }
+}
