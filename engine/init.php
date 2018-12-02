5b49e2bb8e64acf4a5b6fc962f936035d71d7579
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-12-03 00:53:21 +0700

    Update init.php

--- engine/init.php
+++ engine/init.php
@@ -3,4 +3,5 @@
 require_once 'core/props.php';
 require_once 'core/route.php';
 require_once 'core/state.php';
-require_once 'rule/rule.php';
+require_once 'rule/base.php';
+require_once 'rule/database.php';
