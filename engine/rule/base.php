fba3ba625cb6fd4f092c05f14768bf20beff33ea
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-12-03 00:54:44 +0700

    Update base.php

--- engine/rule/base.php
+++ engine/rule/base.php
@@ -36,21 +36,7 @@ $mainRoute = 'home';
 *
 */
 
-//---- Database Config-----//
-$dbServer = 'localhost';
-$dbUser = 'root';
-$dbPassword = '';
-$dbName = 'db_mhs_tes';
-$dbDriver = 'mysql';
 
-//---- Database Wrapper Caption -----///
-$insert = 'INSERT INTO ';
-$select = 'SELECT ';
-$delete = 'DELETE FROM ';
-$update = 'UPDATE ';
-//
-//
-//
 //------ Fungsi ------//
 $stringRandom = "QWERTYUIOPLKJHGFDSAZXCVBNMQWERTYUIOPLKJHGFDSAZXCVBNM";
 $numberRandom = "789456123";
