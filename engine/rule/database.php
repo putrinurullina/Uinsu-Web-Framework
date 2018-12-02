3295a0dc587d3033df2413ba0bf3948e159d1b99
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-12-03 00:55:02 +0700

    Update database.php

--- engine/rule/database.php
+++ engine/rule/database.php
@@ -1,1 +1,17 @@
+<?php
 
+//---- Database Config-----//
+$dbServer = 'localhost';
+$dbUser = 'root';
+$dbPassword = '';
+$dbName = 'db_mhs_tes';
+$dbDriver = 'mysql';
+
+//---- Database Wrapper Caption -----///
+$insert = 'INSERT INTO ';
+$select = 'SELECT ';
+$delete = 'DELETE FROM ';
+$update = 'UPDATE ';
+//
+//
+//
