d1444f74e82ecddb67dde36202cd09087036a679
Haxors Programming Club <44484256+haxorsprogramming@users.noreply.github.com>
2018-12-03 04:24:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 /**
+* Uinsu Web Framework
 *
-*
+* @package	Uinsu Web Framework
+* @author	Haxors Programming Club
+* @license	https://opensource.org/licenses/MIT	MIT License
+* @link	http://haxorsprogrammingclub.id
+* @since	Version 3.5
 */
 require_once 'engine/init.php';
 $props = new Props;
